93eaf2b55b3ad9b1ae1e01c754764ac0d0910fd1
Nerea Fernandez Torres <nfernandez@dam1-pc00.(none)>
2014-01-21 11:06:37 +0100

    Do training

--- solution/01_simple_output/print.cpp
+++ solution/01_simple_output/print.cpp
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define N_MAX 10
 int main(int argc, char *argv[]){
 
+    for(int cantidad =0; cantidad<N_MAX; cantidad++){
+	printf("Soy el primer programa atraves de github.\n");
+    }
     return EXIT_SUCCESS;
 }
