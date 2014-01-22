6943972cb18fd0f88eda4ee0d875442a89b35c1a
Nerea Fernandez Torres <nfernandez@dam1-pc00.(none)>
2014-01-22 13:06:47 +0100

    Third trining

--- solution/03_mean/print3.cpp
+++ solution/03_mean/print3.cpp
@@ -2,6 +2,27 @@
 #include<stdlib.h>
 
 int main(int argc, char *argv[]){
+    int num_cantidad;
+    int media =0;
+    int suma=0;
+    int numero=0;
 
-    return EXIT_SUCCESS;
+    printf("Indique la cantidad de numeros para hacer la media:");
+    scanf(" %i", &num_cantidad);
+if(num_cantidad>0){
+    for(int cantidad =0; cantidad<num_cantidad; cantidad++){
+	printf("Introduce el numero para la media:");
+	scanf(" %i", &numero);
+	suma += numero;
+    }
+    media = suma / num_cantidad;
+    printf("Su media es:%i\n",media);
+
+}else{
+printf("No es posible introducir numero negativo.\n");
+printf("Introduce un numero que contenga signo positivo\n");
+system("./print3");
+}
+
+	return EXIT_SUCCESS;
 }
