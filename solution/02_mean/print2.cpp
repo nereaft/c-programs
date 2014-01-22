3f5ab70b46ac3ddfb6b5f07fbea7346e972f8c71
Nerea Fernandez Torres <nfernandez@dam1-pc00.(none)>
2014-01-22 10:48:47 +0100

    second trining

--- solution/02_mean/print2.cpp
+++ solution/02_mean/print2.cpp
@@ -1,7 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define N_MAX 10
+
 int main(int argc, char *argv[]){
+    int media=0;
+    int numero=0;
+    int suma=0;
+
+    for(int cantidad=0;cantidad<N_MAX;cantidad++){
+	printf("Introduzca el numero para hacer la media (intente que sean enteros):\n");
+	scanf(" %i", &numero);
+	suma += numero;
+    }
+    media = suma / N_MAX;
+
+    printf("el resultado de la media es:%i\n", media);
+
+
+
 
     return EXIT_SUCCESS;
 }
