7e506639785cfdc0b289d8978aa02c3fce00ba43
Nerea Fernandez Torres <nfernandez@dam1-pc00.(none)>
2014-02-13 10:50:33 +0100

    exercices HTML

---
+++ solution/06_square_Border/square.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	int altura = atoi(argv[1]);
+
+	for(int fila=0; fila<altura; fila++){
+		for(int colm=0; colm<altura; colm++){
+			if(fila==0 || colm ==0 || fila ==altura-1 || colm == altura-1){
+				printf("+");
+			}else{
+				printf(" ");
+			}
+
+		}
+printf("\n");	
+}
+	printf("\n");
+	return EXIT_SUCCESS;
+}
