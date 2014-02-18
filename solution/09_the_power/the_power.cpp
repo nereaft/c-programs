49bd0bf2165cc815d198467f72608b85db4cf0bf
Nerea Fernandez Torres <nfernandez@dam1-pc00.(none)>
2014-02-18 09:15:28 +0100

    Changes HTML

---
+++ solution/09_the_power/the_power.cpp
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 0x10
+
+int elevar(int base, int exponente){
+int resultado;
+
+}
+
+
+int main(int argc, char *argv[]){
+int expo = atoi (argv[1]);
+
+elevar(N, expo)
+
+
+
+    return EXIT_SUCCESS;
+}
---
+++ solution/10_print_backwards/backwards.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+const char *frase="The world is a vampire.";
+const char *comienzo= frase;
+const char *final= frase;
+do{
+   comienzo++;
+}while (*comienzo != '\0');
+
+final = comienzo;
+
+do{
+    printf("%c", *final);
+    final--;
+}while (final != frase);
+    
+
+
+
+printf("\n");
+    return EXIT_SUCCESS;
+}
