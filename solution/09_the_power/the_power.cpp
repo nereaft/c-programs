be53a94dd4cfc89478fc0b626aa0dd2ce41b70b8
Nerea Fernandez Torres <nfernandez@dam1-pc00.(none)>
2014-03-12 13:26:34 +0100

    Exercices 9

--- solution/09_the_power/the_power.cpp
+++ solution/09_the_power/the_power.cpp
@@ -1,19 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define N 0x10
-
-int power(int base, int exponent){
-
+#define N 0x10 
+ //crea la matriz de 1 al 16
+void rellenar(int lista_base [N]){
+  for(int contador=0; contador < N; contador++)
+      list [contador]= contador+1;
 }
-void matriz(int list)
-
-int main(int argc, char *argv[]){
-int expo = atoi (argv[1]);
 
 
 
 
 
-    return EXIT_SUCCESS;
-}
+ int  main(int argc, char *argv[]){
+	int lista_base [N]; // Creo la variable con la cantidad de la constante
+	int expo;
+//Introduzco el numero al que elevo la matriz
+	printf("Introduce la cantidad del exponente:  ");
+	scanf(" %i", &expo);
+//se llaman a la funcion
+	rellenar(lista_base);
+	calculo(lista_base, expo);
+
+
+
+
+
+	return EXIT_SUCCESS;
+    }
