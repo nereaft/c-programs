e1839ae09bc131b8b5a6c867b8db6b82b5af4a61
nerea <nerea@ubuntu.(none)>
2014-03-20 20:15:07 +0100

    exercices elevacion

--- solution/09_the_power/the_power.cpp
+++ solution/09_the_power/the_power.cpp
@@ -4,27 +4,30 @@
 #define N 16
 
 void rellenar(int matriz [N]){
-  
-
-
+	for(int numeros = 0; numeros < N; numeros++)
+		matriz[numeros] = numeros+1; 
 }
 
 
-
-
- int  main(int argc, char *argv[]){
+int  main(int argc, char *argv[]){
 	int matriz [N];
+	int resultado = 0 ;
 	int expo;
-
-	printf("Introduce la cantidad del exponente:  ");
+	printf("Introduzca el exponente:  ");
 	scanf(" %i", &expo);
 
 	rellenar(matriz);
-	calculo(lista_base, expo);
+
+	printf("Numeros elevados a %i\n", expo);
+
+	for(int i = 0; i < N; i++){
+		resultado = matriz[i] * matriz[i];
+		printf("%i = %i \n", matriz[i],resultado);
+	}
 
 
 
 
 
 	return EXIT_SUCCESS;
-    }
+}
