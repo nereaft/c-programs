a440cdb256f51023677ccfc20952421dfc3d3c03
Nerea Fernandez Torres <nfernandez@dam1-pc00.(none)>
2014-03-21 13:01:20 +0100

    Exercices 8

--- solution/08_adding/adding.cpp
+++ solution/08_adding/adding.cpp
@@ -1,28 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>  
+#include <stdlib.h>
 
-double calculo(double operador_01,double operador_02){
-    double resultado;   
-    resultado = operador_01 + operador_02;
-    printf("Resultado de su operacion:  %lf", resultado);
+int main(int argc, char *argv[]){
+//Sera el contador donde se almacenan mis numeros
+        double resultado = 0;
+//argv se utiliza como numero limite ya que contiene la cantidad de numeros que tiene la array argv
+	    for (int argumento=1; argumento < argc; argumento++)
+//transforma los argumentos de argv a numeros double
+		resultado += atof(argv[argumento]);
+
+	        printf("%lf\n", resultado);
+
+		    return EXIT_SUCCESS;
 }
 
-
-int main (int argc,char *argv[]){
-
-int num_cantidad;
-
-
-    printf("Cantidad de numeros:  ");
-    scanf(" %i", &num_cantidad);
-   
-for(int repeticion = 0; repeticion < num_cantidad; repeticion++){
-	printf("Introduce numero:   ");
-	scanf(" %.2lf",     
-
-    datos();  
-    calculo(numero1,numero2);
-    printf("\n");
-
-	    return EXIT_SUCCESS;
-	    } 
