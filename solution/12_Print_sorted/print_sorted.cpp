c26b6910a8136198a36a69f0ad27f6ad2755370c
Nerea Fernandez Torres <nfernandez@dam1-pc00.(none)>
2014-03-26 10:30:31 +0100

    ...

--- solution/12_Print_sorted/print_sorted.cpp
+++ solution/12_Print_sorted/print_sorted.cpp
@@ -5,26 +5,52 @@
 
 int main (int argc, char *argv[]){
 
-    const char * list[] = {"yield", "yelp", "yellow" };
+    const char * list[] = {"yield", "yelp", "yellow"};
 
-    int valor_01 = strcmp(list[0],list [1]);
-    int valor_02 =  strcmp(list[0],list [1]);
+    int compara_1_2 = strcmp(list[0],list [1]);
+    int compara_1_3 = strcmp(list[0],list [2]);
+    int compara_2_3 = strcmp(list[1],list [2]);
 
-    if(valor_01 == 0 & 
-	printf(" La palabra %s es igual a la palabra %s ", list[0],list[1]);
-    }else{
-	if(valor > 0)
-	    printf("La palabra %s es mayor que %s ", list[0],list[1]);
-	else
-	    printf("La palabra %s es menor que %s ", list[0],list[1]);
-    }
-
-    printf("\n");
+  if(compara_1_2 == 0){
+    if(compara_1_2 == 0 || compara_1_3 > 0)
+	printf(" %s %s %s \n", list[0], list[1], list[2]);
+    
+    if(compara_1_3 < 0)
+	printf(" %s %s %s \n", list[2], list[0], list[1]);
+   };
 
 
+ if(compara_1_2 < 0){
+    if(compara_2_3 == 0 || compara_1_3 < 0)
+	printf(" %s %s %s \n", list[1], list[2], list[0]);
+    
+    if(compara_2_3 < 0)
+	printf(" %s %s %s \n", list[2], list[1], list[0]);
+   
+ if(compara_2_3 > 0 || compara_1_3 == 0 || compara_1_3 > 0)
+	printf(" %s %s %s \n", list[1], list[0], list[2]);
 
+ };
 
-
+ if(compara_1_2 > 0) {
+    if(compara_1_3 == 0 || compara_2_3 < 0)
+	printf(" %s %s %s \n", list[0], list[2], list[1]);
+    
+    if(compara_1_3 > 0 || compara_2_3 == 0 || compara_2_3 > 0)
+	printf(" %s %s %s \n", list[0], list[1], list[2]);
+   
+    if(compara_1_3 < 0)
+  printf(" %s %s %s \n", list[2], list[1], list[0]);
+  
+  };
+  
+   
+   
+   
+   
+   
+   
+   
     return EXIT_SUCCESS;
 }
 
