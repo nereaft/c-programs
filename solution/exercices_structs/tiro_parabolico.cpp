ebed60ba4a467eb9c1732655d51f603768d6e047
Nerea Fernandez Torres <nfernandez@dam1-pc00.(none)>
2014-04-24 13:01:44 +0200

    Changes structs

---
+++ solution/exercices_structs/tiro_parabolico.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define Pto_inic 5
+
+
+typedef structs{
+    double punto;
+    double velocidad;
+    double aceleracion;
+}Coordenadas;
+
+typedef structs{
+    double x;
+    double y;
+}Posicion;
+
+
+
+void muestra(){/* Muestra por pantalla la x y la y de cada vuelta */
+    /*Termina cuando y es 0 o menor de 0 */
+
+
+}
+
+
+
+
+int main(int argc, char *argv[]){
+
+    int veloc_inic;
+
+    printf("Velocidad inicial: \n");
+    scanf(" %.2lf", &veloc_inic);
+
+    calculo(veloc_inic, );
+
+    
+    
+    
+    
+    muestra();
+
+
+
+    printf("\n");
+    return EXIT_SUCCESS;
+}
+
